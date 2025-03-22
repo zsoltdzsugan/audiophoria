5a48b0d5b6eec5a7424a26fbbdd6652e0077e3ca
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-03-22 09:06:13 +0100

    feat: relation to media

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 class Room extends Model
 {
@@ -14,6 +15,11 @@ class Room extends Model
     public function track(): BelongsTo
     {
         return $this->belongsTo(Track::class);
+    }
+
+    public function media(): HasOneThrough
+    {
+        return $this->hasOneThrough(Media::class, Track::class, 'id', 'id', 'track_id', 'media_id');
     }
 
     protected $guarded = ['id'];
