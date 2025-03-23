d8b8a6f2c41e9864228ea9d89c83c73568d91cfb
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-03-23 07:38:02 +0100

    feat: message event handler

---
+++ app/Events/NewMessageEvent.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NewMessageEvent implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $roomId;
+    public $message;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($roomId, $message)
+    {
+        $this->roomId = $roomId;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn()
+    {
+        return new Channel('room.' . $this->roomId);
+    }
+
+    /**
+     * Get the data to broadcast.
+     *
+     */
+    public function broadcastAs()
+    {
+        return 'new.message';
+    }
+}
