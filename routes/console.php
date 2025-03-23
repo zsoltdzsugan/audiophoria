4e64f7917ddfdcd1a3d04d71a73b353937053140
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-03-23 07:29:15 +0100

    feat: Message model factory migration

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Models\Room;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    Room::where('end_time', '<=', now())->update(['is_active' => false]);
+})->everyMinute();
